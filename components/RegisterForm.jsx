e33e1537e437888aa618dae655a21cc77a430699
danielmartinezs <daniel.martinezs@udem.edu>
2023-11-20 02:06:26 -0600

    payment custom

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -73,7 +73,7 @@ const RegisterForm = () => {
                                 </div>
                                 <div>
                                     <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmar Contraseña</label>
-                                    <input type="confirm-password" name="confirm-password" placeholder="Al menos 8 caracteres" value={confpassword} required onChange={(e) => { setConfPassword(e.target.value) }} class="bg-gray-50 border border-orange-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                    <input type="password" name="confirm-password" placeholder="Al menos 8 caracteres" value={confpassword} required onChange={(e) => { setConfPassword(e.target.value) }} class="bg-gray-50 border border-orange-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                                 </div>
                                 <div class="flex items-start">
                                     <div class="flex items-center h-5">
@@ -83,7 +83,7 @@ const RegisterForm = () => {
                                         <label for="terms" class="font-light text-gray-500 dark:text-gray-300">He leído y acepto los <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Términos y Condiciones</a></label>
                                     </div>
                                 </div>
-                                <button type="submit" class="w-full text-white bg-emerald-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Crear cuenta</button>
+                                <button type="submit" class="w-full text-white bg-emerald-400 hover:bg-primary-green focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Crear cuenta</button>
                                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                                     ¿Ya tienes una cuenta? <a href="login" class="font-medium text-emerald-300 hover:underline dark:text-emerald-300">Inicia sesión aquí</a>
                                 </p>
